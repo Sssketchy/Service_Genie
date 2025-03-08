43fca785b2a18d4b6df87e7bd3b112cfb605088e
Kamalhimself <Kamaleswar2304@gmail.com>
2025-03-08 21:27:14 +0530

    Updated checkout flow and fixed Firebase permissions

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.service_genie"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "29.0.13113456"  // Updated to match Firebase requirements
+    ndkVersion = "27.0.12077973"  // Updated to match Firebase requirements
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
